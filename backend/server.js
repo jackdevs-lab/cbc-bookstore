97c3a69b59852f62b01b6db0b973e138713f8bb3
Ndegwa Jackson <128777440+jackdevs-lab@users.noreply.github.com>
2025-11-26 07:12:22 +0300

    updated frontend url

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const pool = new Pool({
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://www.cbcbookstore.co.ke',
   credentials: true
 }));
 app.use(express.json());
